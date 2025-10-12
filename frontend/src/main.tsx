28d3007421fe010cdbe0c7de00b05bf9f2cbf1c6
ppardus <outbackdriving052@gmail.com>
2025-10-12 03:24:56 +0000

    fix: misuse issue on main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,23 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { createBrowserRouter, RouterProvider, HashRouter} from "react-router-dom";
+import { HashRouter} from "react-router-dom";
 import "./index.css";
 import App from "./App";
-import GeneratePage from "./pages/GeneratePage";
-import QuizPage from "./pages/QuizPage";
-import SummaryPage from "./pages/SummaryPage";
-
-const router = createBrowserRouter([
-  { path: "/", element: <App />, children: [
-    { index: true, element: <GeneratePage /> },
-    { path: "quiz/:quizId", element: <QuizPage /> },
-    { path: "summary/:setId", element: <SummaryPage /> }
-  ]},
-]);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    {/* <RouterProvider router={router} /> */}
     <HashRouter>
       <App />
     </HashRouter>
